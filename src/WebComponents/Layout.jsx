de5a24bc32e0e3fb3b886813e3b5873c433a5972
LAPTOP-CGJ3RLTN\andre <andres_pallares@hotmail.com>
2022-04-15 12:15:31 +0200

    actualizacion

--- src/WebComponents/Layout.jsx
+++ src/WebComponents/Layout.jsx
@@ -39,38 +39,44 @@ function Layout(){
        },[])
 
     return (
+        <div className="container" >
+          <div className='lyt01'>
+              <header className='sm:block bg-blue-500 p-6 '>
+                  
+              </header>
+              <nav className='group   bg-blue-700 shadow-xl leading-none  '>
+                  <button type="button" id="btn-toggle-left" className="nav-mobile ripple push-float-left">
+                    <i className="fa fa-bars fa-2x" ></i>  
+                  </button>
         
-        <div className='lyt01'>
-             <header className='sm:block bg-blue-500 p-6 '>
-                
-             </header>
-             <nav className='group   bg-blue-700 shadow-xl leading-none  '>
-             <button type="button" id="btn-toggle-left" className="nav-mobile ripple push-float-left">
-               <i className="fa fa-bars fa-2x" ></i>  
-             </button>
-  
-             <button type="button" id="btn-toggle-right" className="nav-mobile ripple push-float-right">
-               <i className="fa fa-list fa-2x" ></i>
-             </button>
-             <button type="button" id="btn-toggle-sign-in" className="nav-mobile ripple push-float-right">
-               <i className="fa fa-sign-in fa-2x" ></i>  
-              </button>
-              <MyMenuHorizontal />
-             
-             </nav>
-             <div className='row' >
-                 <aside>
-                   <MyMenuVertical id="MenuVertical01" />
-                 </aside>
-                 <section className='bg-blue-100'>
-                     
-                 </section>
-             </div>
-             <footer className='bg-blue-500'>
-                 
-             </footer>
+                  <button type="button" id="btn-toggle-right" className="nav-mobile ripple push-float-right">
+                    <i className="fa fa-list fa-2x" ></i>
+                  </button>
+                  <button type="button" id="btn-toggle-sign-in" className="nav-mobile ripple push-float-right">
+                    <i className="fa fa-sign-in fa-2x" ></i>  
+                  </button>
+                  
+                  <MyMenuHorizontal />
+              
+              </nav>
+              <div className='row' >
+                  <aside>
+                    <MyMenuVertical id="MenuVertical01" />
+                  </aside>
+                  <section className='bg-blue-100'>
+                      
+                  </section>
+              </div>
+              <footer className='bg-blue-500'>
+                  
+              </footer>
+          </div>
+          <div className="childLayout">
+
+          </div>
+          <div className="childLayout1"></div>
+          <div className="childLyt" ></div>
         </div>
-        
     )
 
 }
